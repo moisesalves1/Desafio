6eef252362ef8694ba6e987af1c367d5e4f3a3c1
Moises Alves <moisesalves1@gmail.com>
2023-04-10 22:41:16 -0300

    Desafio - primeiras paginas

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Blog.Models;
+using Blog.Repositories;
+using Blog.Screens.TagScreens;
+using Dapper.Contrib.Extensions;
+using Microsoft.Data.SqlClient;
+
+namespace Blog
+{
+    class Program
+    {
+        private const string CONNECTION_STRING = @"Server=localhost,1433;Database=Blog;User ID=sa;Password=1q2w3e4r@#$;TrustServerCertificate=True";
+        static void Main(string[] args)
+        {
+            var connection = new SqlConnection(CONNECTION_STRING);
+            connection.Open();
+
+            Load();
+
+            Console.ReadKey();
+            connection.Close();
+            
+        }
+
+        private static void Load(){
+            Console.Clear();
+            Console.WriteLine("Meu Blog");
+            Console.WriteLine("-----------------");
+            Console.WriteLine("O que deseja fazer?");
+            Console.WriteLine();
+            Console.WriteLine("1 - Gestão de usuário");
+            Console.WriteLine("2 - Gestão de perfil");
+            Console.WriteLine("3 - Gestão de categoria");
+            Console.WriteLine("4 - Gestão de tag");
+            Console.WriteLine("5 - Vincular perfil/usuário");
+            Console.WriteLine("6 - Vincular post/tag");
+            Console.WriteLine("7 - Relatórios");
+            Console.WriteLine();
+            Console.WriteLine();
+            var option = short.Parse(Console.ReadLine()!);
+
+            switch (option)
+            {
+                case 4:
+                    MenuTagScreen.Load();
+                    break;
+                default: Load(); break;
+            }
+        }
+    
+    }
+}
