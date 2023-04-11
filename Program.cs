24946b393392ed629daad0dc4673e5f2f06a3436
Moises Alves <moisesalves1@gmail.com>
2023-04-10 23:00:39 -0300

    Compartilhando a conexão

--- Program.cs
+++ Program.cs
@@ -12,13 +12,13 @@ namespace Blog
         private const string CONNECTION_STRING = @"Server=localhost,1433;Database=Blog;User ID=sa;Password=1q2w3e4r@#$;TrustServerCertificate=True";
         static void Main(string[] args)
         {
-            var connection = new SqlConnection(CONNECTION_STRING);
-            connection.Open();
+            Database.Connection = new SqlConnection(CONNECTION_STRING);
+            Database.Connection.Open();
 
             Load();
 
             Console.ReadKey();
-            connection.Close();
+            Database.Connection.Close();
             
         }
 
--- Screens/TagScreens/ListTagScreen.cs
+++ Screens/TagScreens/ListTagScreen.cs
@@ -1,10 +1,23 @@
+using Blog.Models;
+using Blog.Repositories;
+
 namespace Blog.Screens.TagScreens
 {
     public static class ListTagScreen
     {
         public static void Load()
         {
-
+            List();
         }
+
+        private static void List(){
+            var repository = new Repository<Tag>(Database.Connection);
+            var tags = repository.Get();
+            foreach(var item in tags)
+            {
+                Console.WriteLine($"{item.Id} - {item.Name} ({item.Slug})");
+            }
+        }
+            
     }
 }
